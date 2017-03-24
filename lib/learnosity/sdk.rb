3c9ea3ea0d75e1e10f8ae3f61436870958700c67
Olivier Mehani <olivier.mehani@learnosity.com>
2017-03-24 15:56:35 +1100

    [FEATURE] bundle gem learnosity-sdk
    Signed-off-by: Olivier Mehani <olivier.mehani@learnosity.com>

---
+++ lib/learnosity/sdk.rb
@@ -0,0 +1,7 @@
+require "learnosity/sdk/version"
+
+module Learnosity
+  module Sdk
+    # Your code goes here...
+  end
+end
---
+++ lib/learnosity/sdk/version.rb
@@ -0,0 +1,5 @@
+module Learnosity
+  module Sdk
+    VERSION = "0.1.0"
+  end
+end
