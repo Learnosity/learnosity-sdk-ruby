71f63d0183dcb65720bbd03c03b4723c6cdefb7e
Rhiannon Eldridge <rhiannon.eldridge@learnosity.com>
2021-11-23 18:18:39 +1100

    [TEST] Tidy up integration tests

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -11,12 +11,16 @@ RSpec.describe 'Integration Tests' do
   # XXX: The consumer secret should be in a properly secured credential store, and *NEVER* checked in in revision control
   consumer_secret = '74c5fd430cf1242a527f6223aebd42d30464be22'
 
-  TEST_ENV_DOMAIN=''
-  TEST_REGION_DOMAIN='.learnosity.com'
   if !ENV['ENV'].nil? && ENV['ENV'] != 'prod'
     TEST_ENV_DOMAIN=".#{ENV['ENV']}"
-  elsif !ENV['REGION'].nil?
+  else
+    TEST_ENV_DOMAIN=''
+  end
+
+  if !ENV['REGION'].nil?
     TEST_REGION_DOMAIN="#{ENV['REGION']}" # region is of the form -<REGION>.learnosity.com
+  else
+    TEST_REGION_DOMAIN='.learnosity.com'
   end
 
   if !ENV['VER'].nil?
@@ -28,30 +32,29 @@ RSpec.describe 'Integration Tests' do
   base_url = "https://data#{TEST_ENV_DOMAIN}#{TEST_REGION_DOMAIN}/#{TEST_VERSION_PATH}"
 
   context 'Data API' do
-
-    data_request = { 'limit' => 100 }
-
     it "can retrieve data from #{base_url}" do
-	init = Learnosity::Sdk::Request::Init.new(
-		'data',
-		security_packet,
-		consumer_secret,
-		data_request
-	)
+      data_request = { 'limit' => 100 }
 
-	request = init.generate
+      init = Learnosity::Sdk::Request::Init.new(
+        'data',
+        security_packet,
+        consumer_secret,
+        data_request
+      )
 
-	itembankUri = URI("#{base_url}/itembank/items")
+      request = init.generate
 
-	res = Net::HTTP.post_form(itembankUri, request)
+      itembankUri = URI("#{base_url}/itembank/items")
 
-	expect(res.code).to eq('200')
+      res = Net::HTTP.post_form(itembankUri, request)
 
-	response = JSON.parse(res.body)
+      expect(res.code).to eq('200')
 
-	expect(response['meta']['next']).to_not be nil
-	expect(response['meta']['records']).to_not be nil
-        expect(response['meta']['records']).to be > 0
+      response = JSON.parse(res.body)
+
+      expect(response['meta']['next']).to_not be nil
+      expect(response['meta']['records']).to_not be nil
+      expect(response['meta']['records']).to be > 0
     end
   end
 end
