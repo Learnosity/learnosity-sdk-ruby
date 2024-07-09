9bdb14a96d6dc40511f3bc11ee057daf53c22b50
Markus <markus.liang@learnosity.com>
2024-07-09 13:46:38 +0100

    [FEATURE] Add support for Authoraide API LRN-44066

--- docs/quickstart/lrn-sdk-rails/config/routes.rb
+++ docs/quickstart/lrn-sdk-rails/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'index/index'
   get 'questions/index', as: 'questions_index'
   get 'author/index' , as: 'author_index'
+  get 'authoraide/index' , as: 'authoraide_index'
   get 'reports/index', as: 'reports_index'
   get 'items/index', as: 'items_index'
 #   get 'abc' , to: "index#index"
